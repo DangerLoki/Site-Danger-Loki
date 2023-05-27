06a85e3edfed3df5a28fb1caa9a6664da86f8cb3
Danger Loki <andremicael13@hotmail.com>
2023-05-27 19:37:33 -0300

    Tela de login

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnUser');
+const iconClose = document.querySelector('.icon-close');
 
-menu.onclick= () => {
-    menu.classList.toggle('bx-x');
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('open');
+};
 
-    navbar.classList.toggle('open');
-}
+registerLink.addEventListener('click', () => {
+  wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+  wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
